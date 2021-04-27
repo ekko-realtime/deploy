38bc42a15bd27dd35cf441650e9972f8edd685b7
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-04-27 18:15:12 -0400

    add Drew's CLI code

--- bin/deploy.js
+++ bin/deploy.js
@@ -14,6 +14,7 @@ class EkkoApp extends cdk.App {
       cluster: sharedResources.cluster,
       redis: sharedResources.redis,
       vpc: sharedResources.vpc,
+      s3: sharedResources.s3,
     });
   }
 }
