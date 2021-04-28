e4d8ba84fc90f1fce90e0b8f26dfaf5998d76bbe
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-04-28 15:00:21 -0400

    delete cdk deployment code to make smaller deploy
    left an s3 bucket and a VPC

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,21 +1,13 @@
 #!/usr/bin/env node
 
 const cdk = require('@aws-cdk/core');
-const { EkkoStack, SharedResources } = require('../lib/deploy-stack');
+const { SharedResources } = require('../lib/deploy-stack');
 
 // A construct which represents the entire CDK app
 class EkkoApp extends cdk.App {
   constructor() {
     super();
-
     const sharedResources = new SharedResources(this, "shared-resources");
-
-    const server = new EkkoStack(this, "ekko-server", {
-      cluster: sharedResources.cluster,
-      redis: sharedResources.redis,
-      vpc: sharedResources.vpc,
-      s3: sharedResources.s3,
-    });
   }
 }
 
