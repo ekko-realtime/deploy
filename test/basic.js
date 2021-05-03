de0a8474a589c286fe2f70cc6ac4cdcf1f3b2fda
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-05-03 16:05:26 -0400

    add ECS testing

--- test/basic.js
+++ test/basic.js
@@ -4,14 +4,31 @@ const haveResource = assert.haveResource;
 
 const cdk = require('@aws-cdk/core');
 const Deploy = require('../lib/deploy-stack');
+const Redis = require('../lib/redis');
 const tap = require('tap');
+const fullStack = require('../bin/deploy');
 
 tap.test('Test resource presence', async subTest => {
   const app = new cdk.App();
   // WHEN
-  const stack = await new Deploy.SharedResources(app, 'MyTestStack');
+  const stack = await new Deploy.SharedResources(app, 'SharedResourcesStack');
   // THEN
   expectCDK(stack).to(haveResource("AWS::S3::Bucket",{}));
   expectCDK(stack).to(haveResource("AWS::EC2::VPC",{}));
   expectCDK(stack).to(haveResource("AWS::ECS::Cluster",{}));
+});
+
+tap.test('Test resource presence', async subTest => {
+  const app = new cdk.App();
+  // WHEN
+  const sr = await new Deploy.SharedResources(app, 'SharedResourcesStack');
+  const stack = await new Deploy.EkkoStack(app, "ekko-server", {
+    cluster: sr.cluster,
+    redis: sr.redis,
+    vpc: sr.vpc,
+    s3: sr.s3,
+  });
+
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::ECS::TaskDefinition",{}));
 });
