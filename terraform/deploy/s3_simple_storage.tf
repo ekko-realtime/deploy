a2a2321397ebcd62fdb389383e7cb380d949d77f
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-06-10 17:22:58 +0200

    simplify shared resources bucket creation

--- terraform/deploy/s3_simple_storage.tf
+++ terraform/deploy/s3_simple_storage.tf
@@ -1,16 +1,5 @@
-resource "aws_s3_bucket" "shared_resources_ekkobucket331a3a61_dexqbg3bet5j" {
-  tags = {
-    "aws-cdk:auto-delete-objects" = "true"
-    service                       = "ekko"
-  }
+resource "aws_s3_bucket" "shared_resources_ekko" {
 
-  tags_all = {
-    "aws-cdk:auto-delete-objects" = aws_vpc.shared_resources_ekkosharedvpc.enable_dns_hostnames
-    service                       = "ekko"
-  }
-
-  arn            = "arn:aws:s3:::shared-resources-ekkobucket331a3a61-dexqbg3bet5j"
-  bucket         = "shared-resources-ekkobucket331a3a61-dexqbg3bet5j"
-  hosted_zone_id = "Z3BAZG2TWCNX0D"
+  bucket = "shared_resources_ekko"
 }
 
