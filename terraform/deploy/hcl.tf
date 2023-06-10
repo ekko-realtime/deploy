1c9897890c77c819304e498abeee79ab5d1265ab
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-06-10 17:02:25 +0200

    set eu-north-1 as default

--- terraform/deploy/hcl.tf
+++ terraform/deploy/hcl.tf
@@ -1,15 +1,14 @@
-provider "aws" {
-}
-
-
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = ">= 5.2.0"
     }
-
   }
+}
 
-  required_version = ">= 1.0"
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-north-1"
 }
 
