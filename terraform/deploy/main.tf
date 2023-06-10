ab9f5066b21bf73e5728c83ec74b008bc9cca26a
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-06-10 17:27:47 +0200

    refactor

--- terraform/deploy/main.tf
+++ terraform/deploy/main.tf
@@ -17,3 +17,14 @@ provider "aws" {
   }
 }
 
+
+# SHARED RESOURCES
+
+resource "aws_s3_bucket" "shared_resources_ekko" {
+
+  bucket = "shared_resources_ekko"
+}
+
+
+# EKKO STACK
+
--- terraform/deploy/s3_simple_storage.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_s3_bucket" "shared_resources_ekko" {
-
-  bucket = "shared_resources_ekko"
-}
-
